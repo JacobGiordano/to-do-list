49645af011c8cb84054210641ee546910609ea95
JacobGiordano <jacob.giordano12@gmail.com>
2021-07-09 05:56:18 -0700

    Add starter task of playing with the nav options

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ if (JSON.parse(localStorage.getItem("to-do-data")) !== null && storedData.length
   const newProj = new Project(newProjId);
   newProj.title = "Starter Project";
 
-  const taskList = ["Update this project's title", "Expand/collapse a project by clicking it or the down arrow in its header", "Click the + in this project's header to add a new task", "Change a task's title", "Click the 3 dots to the right to edit a task", "Change the priority of a task by clicking the 'Set priority' button, or the gray bar to the left","Add notes to a task", "Delete a task by clicking the trash can", "Add a new project by clicking the + in the lower-right", "Delete a project by clicking the trash can in its header"];
+  const taskList = ["Update this project's title", "Expand/collapse a project by clicking it or the down arrow in its header", "Click the + in this project's header to add a new task", "Change a task's title", "Click the 3 dots to the right to edit a task", "Change the priority of a task by clicking the 'Set priority' button, or the gray bar to the left","Add notes to a task", "Delete a task by clicking the trash can", "Add a new project by clicking the + in the lower-right", "Delete a project by clicking the trash can in its header", "Open the nav and play with the options"];
   for (let i=0; i < taskList.length; i++) {
     const newTask = new Task(uuidv4());
     newTask.text = taskList[i];
